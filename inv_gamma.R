00cfb017cad26da22d5a002cab432a4c498d8eaa
Andrey776 <61335235+Andrey776@users.noreply.github.com>
2021-02-10 09:30:51 -0700

    close to final

--- inv_gamma.R
+++ inv_gamma.R
@@ -3,8 +3,8 @@ corrplot(M, type = "upper", tl.col = "steel blue")
 
 
 library(invgamma)
-x = seq(0.0001, 1, length= 10000)
-plot(x, dinvgamma(x, shape =10, scale = 4), type = "l", col = "red")
+x = seq(0.0001, .5, length= 10000)
+plot(x, dinvgamma(x, shape = 0.01, rate = 0.01), type = "l", col = "red")
 lines(x, dinvgamma(x, shape =10, scale = 2), col = "blue")
 lines(x, dinvgamma(x, shape =10, scale = 1), col = "green")
 
